62fa6026a6220283a994e749a8539be9e247d05a
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2023-02-07 23:48:00 +0530

    Updaed movie admin field to display average rating

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -3,8 +3,8 @@ from .models import Movie
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'calculate_ratings_count']
-    readonly_fields = ['calculate_ratings_count', 'rating_avg_display']
+    list_display = ['__str__', 'rating_count',  'rating_avg', 'rating_last_updated']
+    readonly_fields = ['rating_avg', 'rating_count', 'rating_avg_display']
 
 
 admin.site.register(Movie, MovieAdmin)
