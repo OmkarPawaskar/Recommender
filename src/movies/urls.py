6607cf303f74b492e272e0ee2c5a2583246aaeb0
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2024-03-06 01:03:03 +0530

    24 - Infinite Rating Flow with Django & HTMX

--- src/movies/urls.py
+++ src/movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.movie_list_view),
+    path('infinite/', views.movie_infinite_rating_view),
     path('<int:pk>/', views.movie_detail_view), #<int:pk> passes that part of the url to the view as a parameter by that name.
 ]
--- src/movies/views.py
+++ src/movies/views.py
@@ -1,5 +1,3 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.views import generic
 
 # Create your views here.
@@ -71,3 +69,18 @@ class MovieDetailView(generic.ListView):
 
 movie_detail_view = MovieDetailView.as_view()
 
+class MovieInfiniteRatingView(MovieDetailView):
+    def get_object(self):
+        user= self.request.user
+         # exclude_ids = []
+        # if user.is_authenticated:
+        #     exclude_ids = [x.object_id for x in user.rating_set.filter(active=True)]
+        return Movie.objects.all().order_by("?").first() #randomizes the order of the retrieved objects.
+    
+    def get_template_names(self):
+        request = self.request
+        if request.htmx:
+             return ['movies/snippet/infinite.html']
+        return ['movies/infinite-view.html']
+    
+movie_infinite_rating_view = MovieInfiniteRatingView.as_view()
