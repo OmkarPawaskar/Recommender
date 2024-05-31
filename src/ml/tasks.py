3b67942e6405b3915f8a1851cd1a9256b3654fe6
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2024-05-31 08:58:29 +0530

    37 - Trigger ML Predictions Per User Activity

--- src/ml/tasks.py
+++ src/ml/tasks.py
@@ -24,7 +24,7 @@ def batch_users_prediction_task(users_ids=None, start_page=0, offset=250, max_pa
         users_done = recently_suggested.get(f"{movie_id}") or []
         for u in users_ids:
             if u in users_done:
-                print(movie_id, 'is done for', u , 'user')
+                #print(movie_id, 'is done for', u , 'user')
                 continue
             pred = model.predict(uid=u, iid=movie_id).est
             #print(u, movie_id, pred)
@@ -40,7 +40,3 @@ def batch_users_prediction_task(users_ids=None, start_page=0, offset=250, max_pa
     Suggestion.objects.bulk_create(new_suggestion, ignore_conflicts=True)
     if end_page < max_pages:
         return batch_users_prediction_task(start_page=end_page-1)
-
-@shared_task
-def batch_single_user_predictions_task(user_id=1, start_page=0, offset=250, max_pages=100_000):
-    return batch_users_prediction_task(users_ids=[user_id], start_page=start_page, offset=offset, max_pages=max_pages)
