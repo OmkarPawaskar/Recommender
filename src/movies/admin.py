893b66a6dd6acbbf199728610acdbb180d17bd87
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2024-06-01 23:18:39 +0530

    39 - Movie Embedding Idx Field and Task

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -3,8 +3,8 @@ from .models import Movie
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'rating_count',  'rating_avg', 'rating_last_updated']
-    readonly_fields = ['rating_avg', 'rating_count', 'rating_avg_display']
+    list_display = ['__str__', 'idx', 'rating_avg', 'rating_count']
+    readonly_fields = ['idx', 'rating_avg', 'rating_count']
     search_fields = ['id']
 
 admin.site.register(Movie, MovieAdmin)  
