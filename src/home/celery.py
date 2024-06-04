901474604848cc93e53412790d1d1aebfafb2678
Omkar Pawaskar <omkarpawaskar96@hotmail.com>
2024-06-05 00:54:54 +0530

    41 - Schedule for ML Training, ML Inference, Movie IDX Updates, and Exports

--- src/home/celery.py
+++ src/home/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
 
@@ -17,9 +18,26 @@ app.conf.beat_schedule = {
         'schedule' : 60*30, #30 mins
         'kwargs' : {"count" : "20_000"}
     },
-    "run_rating_export_every_hour": {
-        'task' : 'export_rating_dataset',
-        'schedule' : 60*60, #1hr
-    }
+    "daily_movie_idx_refresh": {
+        "task": "movies.tasks.update_movie_position_embedding_idx",
+        "schedule": crontab(hour=1, minute=0)
+    },
+    "daily_rating_dataset_export": {
+        "task": "export_rating_dataset",
+        "schedule": crontab(hour=1, minute=30)
+    },
+    "daily_rating_dataset_export": {
+        "task": "export_movies_dataset",
+        "schedule": crontab(hour=2, minute=15)
+    },
+    "daily_train_surprise_model": {
+        "task": "ml.tasks.train_surprise_model_task",
+        "schedule": crontab(hour=3, minute=0)
+    },
+    "daily_model_inference": {
+        "task": "ml.tasks.batch_users_prediction_task",
+        "schedule": crontab(hour=4, minute=30),
+        "kwargs": {"max_pages": 5000, "offset": 200}
+    },
 }
 
--- src/suggestions/admin.py
+++ src/suggestions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Suggestion 
+
+class SuggestionAdmin(admin.ModelAdmin):
+    list_display = ['content_object', 'object_id', 'user', 'value']
+    search_fields = ['user__username']
+    raw_id_fields = ['user']
+    readonly_fields = ['content_object', 'timestamp']
+
+admin.site.register(Suggestion, SuggestionAdmin)
